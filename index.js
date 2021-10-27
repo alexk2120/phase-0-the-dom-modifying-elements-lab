b8af4508fd4546082984815adff79fe7d6f103b1
alexander.Khatamov <alexk2120@gmail.com>
2021-10-27 12:14:01 -0400

    phase1 submission

--- index.js
+++ index.js
@@ -7,23 +7,29 @@ for (let i = 0; i < 3; i++) {
   li.textContent = (i + 1).toString();
   ul.append(li);
 }
-
 element.append(ul);
-const main = document.getElementById("main");
-main.innerHTML = <h1> Whooodddyyy whoooo </h1>
+
 const main = document.getElementById("main");
 main.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
+
 const element = document.getElementById("main");
 element.style.height = "300px";
 element.style.backgroundColor = "#27647B";
-'#27647B'
+
 element.textContent = "You've changed what's on the screen!";
 element.style.fontSize = "24px";
 element.style.marginLeft = "30px";
 element.style.lineHeight = 2;
+
 element.className = "pet-listing dog";
 element.classList.remove("dog");
 element.classList.add("cat", "sale");
 const ul = document.getElementsByTagName("ul")[0];
 const secondChild = ul.querySelector("li:nth-child(2)");
 ul.removeChild(secondChild);
+
+element.remove(main)
+
+const newHeader = document.createElement(h1 id='victory')
+
+document.body.append(newHeader)
